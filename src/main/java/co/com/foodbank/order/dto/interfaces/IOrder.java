96132b20468e95e43b7fc287f223a5d02dc6edfd
mlondong <mauricio.londono@gmail.com>
2021-08-13 14:44:11 -0500

    change packages

--- src/main/java/co/com/foodbank/order/dto/interfaces/IOrder.java
+++ src/main/java/co/com/foodbank/order/dto/interfaces/IOrder.java
@@ -3,7 +3,7 @@ package co.com.foodbank.order.dto.interfaces;
 import java.util.Collection;
 import java.util.Date;
 import co.com.foodbank.message.dto.interfaces.IMessage;
-import co.com.foodbank.packaged.dto.IPackaged;
+import co.com.foodbank.packaged.dto.interfaces.IPackaged;
 import co.com.foodbank.user.interfaces.IUser;
 
 /**
